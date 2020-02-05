aec0d91289347031a676a7134c6e3ea60d1120da
szpakucjusz <lukaszszpakowski2@gmail.com>
2020-02-05 17:36:49 +0100

    [chore] - documentation

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -23,24 +23,31 @@ class ImportController extends Controller
 
     public function index()
     {
-        // non-good-code only for viewing purposes. Good code is in App\Console\Commands\ImportJobOffers
+        // non-good-code only for viewing purposes. Good code is in app/Console/Commands/ImportJobOffers.php
         print_r("<pre>");
-        print_r('Brzydkie Wyświetlanie tylko dla celów sprawdzenia czy się zaimportowało.');
+        print_r(strtoupper('Brzydkie Wyświetlanie tylko dla celów sprawdzenia czy się zaimportowało. Sprawdzanie kodu należy zacząć od przeczytania pliku readme.md i przejrzenia kodu: app/Console/Commands/ImportJobOffers.php'));
         print_r("</pre>");
         $jobOffers = JobOffer::with('cities')->get();
-        foreach ($jobOffers as $jobOffer) {
-            print_r("<pre>");
-            print_r('Tytuł oferty: ' . $jobOffer->title);
-            print_r("</pre>");
-            print_r("<pre>");
-            print_r('miasta, których dotyczy oferta:');
-            print_r("</pre>");
-            foreach ($jobOffer->cities as $city) {
+        if (count($jobOffers) > 0) {
+            foreach ($jobOffers as $jobOffer) {
                 print_r("<pre>");
-                print_r($city->name . ', ');
+                print_r('Tytuł oferty: ' . $jobOffer->title);
                 print_r("</pre>");
+                print_r("<pre>");
+                print_r('miasta, których dotyczy oferta:');
+                print_r("</pre>");
+                foreach ($jobOffer->cities as $city) {
+                    print_r("<pre>");
+                    print_r($city->name . ', ');
+                    print_r("</pre>");
+                }
             }
+        } else {
+            print_r("<pre>");
+            print_r('Jeszcze nie ma zaimportowanych ofert pracy.');
+            print_r("</pre>");
         }
+
         return view('welcome');
     }
 
